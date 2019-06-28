83edcdb104f2fe7cdbfcaaa11e6e1d71dd0df99a
smortime <schuymortimer@gmail.com>
2019-06-27 19:57:51 -0700

    - removed reviews code - added working set preferences endpoint

--- coffee-champ-impl/src/main/scala/com/smort/coffeechamp/impl/CoffeeChampServiceImpl.scala
+++ coffee-champ-impl/src/main/scala/com/smort/coffeechamp/impl/CoffeeChampServiceImpl.scala
@@ -1,19 +1,26 @@
 package com.smort.coffeechamp.impl
 
 import akka.Done
-import com.smort.coffeechamp.api
-import com.smort.coffeechamp.api.{CoffeeChampService, CoffeeReview}
+import com.smort.coffeechamp.api.{CoffeeChampService, CoffeePreferences}
 import com.lightbend.lagom.scaladsl.api.ServiceCall
-import com.lightbend.lagom.scaladsl.persistence.{EventStreamElement, PersistentEntityRegistry}
+import com.lightbend.lagom.scaladsl.api.transport.BadRequest
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntityRegistry
+
+import scala.concurrent.ExecutionContext
 
 /**
   * Implementation of the CoffeeChampService.
   */
-class CoffeeChampServiceImpl(persistentEntityRegistry: PersistentEntityRegistry) extends CoffeeChampService {
+class CoffeeChampServiceImpl(persistentEntityRegistry: PersistentEntityRegistry)(implicit ec: ExecutionContext) extends CoffeeChampService {
 
-  def submitReview(name: String): ServiceCall[CoffeeReview, Done] = ServiceCall { request =>
-    val ref = persistentEntityRegistry.refFor[CoffeeChampEntity](name)
+  private def entityRef(id: String) =
+    persistentEntityRegistry.refFor[CoffeeChampEntity](id)
 
-    ref.ask(SubmitReviewCommand(request.name, request.rating, request.review))
+  override def setPreferences(id: String): ServiceCall[CoffeePreferences, Done] = ServiceCall { request =>
+    entityRef(id)
+      .ask(SetPreferences(request.preferences))
+      .recover {
+        case CoffeeChampException(message) => throw BadRequest(message)
+      }
   }
 }
